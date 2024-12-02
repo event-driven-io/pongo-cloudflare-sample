f73f338fde607ca74302626d7912a1375fa635f6
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-02 09:10:01 +0100

    Updated Pongo to 0.16.3

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,12 @@ export default {
 
 			const now = new Date().toISOString();
 
-			const {insertedId } = await users.insertOne({_id: now, name: `test-${now}`})
+			const {insertedId } = await users.insertOne({_id: now, name: `test-${now}`});
 
 			// Test query
-			const user = await users.findOne({_id: insertedId!});
+			const {_version, ...user } = await users.findOne({_id: insertedId!});
+
+			console.log(JSON.stringify(user));
 
 			// Returns result rows as JSON
 			return Response.json({ result: user });
